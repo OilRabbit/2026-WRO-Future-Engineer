205fa739ed1d16f60f1d6f6cff10eb04c700cb31
marco94487 <marco.94487@gmail.com>
2026-06-17 17:31:50 +0800

    OC1 test

--- src/raspberrypi/main.py
+++ src/raspberrypi/main.py
@@ -41,9 +41,9 @@ recorded_time = 0
 
 #Checkpoints (default as clockwise case)
 angle = 0 #steering percentage
-turn_indi_1 = [320, 75] #check when to turn
-turn_indi_2 = [320, 105] #check when to turn
-sector_indi = [[270, 75], [370, 75]] #check when is sector
+turn_indi_1 = [310, 80] #check when to turn
+turn_indi_2 = [310, 110] #check when to turn
+sector_indi = [[260, 80], [360, 80]] #check when is sector
 turn_time = 0
 
 front_turning_point = [320, 70]
@@ -110,10 +110,7 @@ try:
 				#Format eg: esp.send_command("<speed>, <streering percentage>, <distance(-1 when not needed)>, <what to do>")
 				#The first sector
 				if state == States.FIRST_SECTOR:
-					if track["center_x"] != 0:
-						angle_temp = (track["center_x"]-310)*abs(track["center_x"]-310)/100
-						if angle_temp < 90 and angle_temp > -90:
-							angle = angle_temp
+					angle = (track["center_x"]-310)*abs(track["center_x"]-310)/100
 					esp.send_command("12, " + str(angle) + ", -1, move forward")
 					time.sleep(0.025)
 					if get_track_distance(turn_indi_1[0], turn_indi_1[1])[0] == False and get_track_distance(turn_indi_2[0], turn_indi_2[1])[0] == True and track["center_x"] != 0:
@@ -172,8 +169,8 @@ try:
 						buffer = angle/5
 						for i in range (4):
 							angle -= buffer
-							esp.send_command("10, " + str(angle) +", -1, turn")
-							time.sleep(0.02)
+							esp.send_command("12, " + str(angle) +", -1, turn")
+							time.sleep(0.005)
 						if num_of_turn == 12:
 							state = States.LAST_RUN
 							print("last")
